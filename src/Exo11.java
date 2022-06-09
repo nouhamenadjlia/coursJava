6fc095e48c50aa5a6eabdf21ab1f3c0fdeea86d7
nouhamenadjlia <nouha.menadjlia@hotmail.fr>
2022-06-10 00:08:40 +0200

    mrajout boucle while Exo11

--- src/Exo11.java
+++ src/Exo11.java
@@ -4,13 +4,20 @@ public class Exo11 {
         Scanner input = new Scanner(System.in);
         System.out.print("Donnez le nombre a calculer :");
         int numero = input.nextInt();
-        int reponse = 1;
-        System.out.print("Factorielle de " +numero+ " c'est : 1");
-        for (int i = 2; i<=numero;i++)
-        {
-            reponse*=i;
-            System.out.print(" x " + i);
+
+        while (numero<0){
+            System.out.print("veuillez entrer un nombre positif :");
+            numero = input.nextInt();
         }
-        System.out.print(" = " + reponse );
+        if (numero >1){
+            int fact=1;
+            System.out.print("Factorielle de " +numero+ " c'est : "+1);
+            for (int i = 2; i<=numero; i++) {
+                fact*=i;
+                System.out.print(" x " + i+"="+fact);
+            }
+        }else if (numero == 0){
+            System.out.print("Factorielle de " +numero+ " c'est : "+1);
+        }
     }
 }
